0fe62606f57f4b5aa5c07b1db9d2883860ce8354
antoniogiachin <giachinantonio@outlook.com>
2022-03-03 12:51:21 +0100

    push

---
+++ src/components/myHeader.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="container">
+
+        <!-- navbar bootstrap -->
+        <nav class="navbar navbar-expand-lg ">
+            <div class="container-fluid">
+
+                <!-- logo dimensioni width 50% -->
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/img/dark-logo.png" class="w-50" alt="navbar-logo">
+                </a>
+                
+                <!-- lista link dinamica, ad ogni link in after fontawasome freccia giu' -->
+                <ul class="navbar-nav ms-auto me-3">
+                    
+                    <li class="nav-item ">
+                        <a class="nav-link text-dark ms_nav-link" href="#">Link</a>
+                    </li>
+
+                    <li class="nav-item ">
+                        <a class="nav-link text-dark" href="#">Link</a>
+                    </li>
+
+                    <li class="nav-item ">
+                        <a class="nav-link text-dark" href="#">Link</a>
+                    </li>
+
+                    <li class="nav-item ">
+                        <a class="nav-link text-dark" href="#">Link</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">
+                            <i class="fa-regular fa-circle-user"></i>
+                        </a>
+                    </li>
+                                                            
+                </ul>
+
+                <!-- form stilizzato da me  -->
+                <form class="d-flex align-items-center px-2 rounded-2 ms_form">
+                    <input class="form-control me-2 bg-transparent border-0 ms_input" type="search" placeholder="Search..." aria-label="Search">
+                    <i class="fa-solid fa-magnifying-glass ms_search-icon"></i>    
+                </form>
+                
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+// richiamo varibili create da me
+@import "../assets/style/variables.scss";
+
+.ms_nav-link::after{
+    display: inline-block;
+    text-rendering: auto;
+    -webkit-font-smoothing: antialiased;
+    font: var(--fa-font-solid);
+    content: "\f078";
+    font-size: 0.7em;
+    margin: 0px 5px;
+}
+
+.ms_form {
+    background-color: $colorLightGrey;
+    
+    .ms_input{
+        outline: none !important;
+
+    }
+
+    .ms_search-icon{
+        color: $colorGreen;
+    }
+}
+</style>
